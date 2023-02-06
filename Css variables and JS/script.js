d1024e33e580ebc8aa5c4e2590b6ac42a5b35f45
Vaibhav Singh <vaibhavsinghr.08@gmail.com>
2023-02-07 01:36:57 +0530

    Updated project

---
+++ Css variables and JS/script.js
@@ -0,0 +1,8 @@
+const inputs = document.querySelectorAll('input');
+
+function updateValues(){
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', updateValues))
